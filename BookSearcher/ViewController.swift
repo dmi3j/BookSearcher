2ca06a11071f60c7166baf16a689f6fc72dc3024
Dmitrijs Beloborodovs <dmitrijs.beloborodovs@citadele.lv>
2021-01-25 16:07:49 +0200

    Empty project added

---
+++ BookSearcher/ViewController.swift
@@ -0,0 +1,11 @@
+//  Copyright © 2021 Dmitrijs Beloborodovs. All rights reserved.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
